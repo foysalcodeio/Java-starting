8d60171647c369cdfa7ee5365f401ff0a3971f0a
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2025-11-29 23:54:10 +0600

    Create array.java

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+       int[] arr = {3, 6, 9, 10, 30};
+       for ( int i : arr){
+           System.out.println(i);
+       }
+
+
+
+        scanner.close();
+    }
+}
