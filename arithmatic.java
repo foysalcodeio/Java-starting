787fc9150ef191cd6dd491474f0e5221c981f8a3
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2025-11-30 12:56:29 +0600

    Create arithmatic.java

---
+++ arithmatic.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What would you like to buy : ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price each food : ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like : ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+        System.out.println("You have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();
+    }
+}
